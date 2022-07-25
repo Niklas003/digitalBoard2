e4bdf8ea8826eb7b031ce257d3a885d8ca8cec42
Niklas <nlabitzke77@gmail.com>
2022-07-25 21:40:37 +0200

    feat: fixed issue in S Bahn Dialog that showed ETA even if train is not calling at the Stations Treptower Park or Schöneweide

--- src/app/sbahn-dialog/sbahn-dialog.component.ts
+++ src/app/sbahn-dialog/sbahn-dialog.component.ts
@@ -17,6 +17,8 @@ export class SBahnDialogComponent implements OnInit {
   treptowArrival: any;
   schoeneweide: boolean = false;
   schoeneweideArrival:any;
+  gruenbergBefore: boolean = false;
+  gruenbergAfter:boolean = false;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: TrainData, public gruenbergDet: GruenbergDetailService) { }
 
@@ -33,13 +35,19 @@ export class SBahnDialogComponent implements OnInit {
     this.gruenbergDet.getGruenbergDetails(this.data.id).subscribe(
       data => {
         for(const d of (data.stopovers as any)){
-          if(d.stop.name == "Berlin-Schöneweide (S)" && !this.schoeneweide){ //look if the train stops at schöneweide
+          if(d.stop.name == "Berlin Grünbergallee" && !this.gruenbergBefore && !this.schoeneweide && !this.treptow){  //check if the stop Grünbergallee comes before Treptower Park or Schöneweide
+            this.gruenbergBefore = true;
+            if(this.gruenbergBefore && !this.schoeneweide && !this.treptow)   //if this then Grünbergallee comes after Schöneweide or Treptower Park
+              this.gruenbergAfter = true;
+          }
+
+          if(d.stop.name == "Berlin-Schöneweide (S)" && !this.schoeneweide && this.gruenbergBefore){ //look if the train stops at schöneweide
           this.schoeneweide = true;
           this.schoeneweideArrival = new Date(d.departure);
           this.schoeneweideArrival =  this.schoeneweideArrival.setMinutes(this.schoeneweideArrival.getMinutes() + 28);
         }
 
-          if(d.stop.name == "Berlin Treptower Park" && !this.treptow){  //look if train stops at Treptower Park
+          if(d.stop.name == "Berlin Treptower Park" && !this.treptow && this.gruenbergBefore){  //look if train stops at Treptower Park
             this.treptow = true;
             this.treptowArrival = new Date(d.departure);
             this.treptowArrival =  this.treptowArrival.setMinutes(this.treptowArrival.getMinutes() + 5);
