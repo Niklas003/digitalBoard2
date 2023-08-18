696fe9d361d1165dbef381c9678cbc6df1015a6a
Niklas <62644517+Niklas003@users.noreply.github.com>
2023-08-18 14:14:04 +0200

    feat: fixed spelling

--- src/app/hauptbahnhof/hauptbahnhof.component.ts
+++ src/app/hauptbahnhof/hauptbahnhof.component.ts
@@ -4,84 +4,87 @@ import { MatDialog } from '@angular/material/dialog';
 import { HauptbahnhofDetailsComponent } from '../hauptbahnhof-details/hauptbahnhof-details.component';
 import { Subscription } from 'rxjs';
 
-
 @Component({
   selector: 'app-hauptbahnhof',
   templateUrl: './hauptbahnhof.component.html',
-  styleUrls: ['./hauptbahnhof.component.scss']
+  styleUrls: ['./hauptbahnhof.component.scss'],
 })
 export class HauptbahnhofComponent implements OnInit {
-
   stationData: any = [];
-  name:any;
-  hauptbahnhofDataSubscription:Subscription;
-  intervall:any;
-  timeUntilRequested:any;
+  name: any;
+  hauptbahnhofDataSubscription: Subscription;
+  interval: any;
+  timeUntilRequested: any;
 
-  constructor(private api: StationService, public dialog: MatDialog) { }
+  constructor(private api: StationService, public dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.getStationData();
     this.updateData();
   }
 
-  ngOnDestroy(): void{
-    clearInterval(this.intervall);
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
     this.hauptbahnhofDataSubscription.unsubscribe();
   }
 
-  updateData(){
-    this.intervall = setInterval(()=> { this.getStationData() }, 30 * 1000);
+  updateData() {
+    this.interval = setInterval(() => {
+      this.getStationData();
+    }, 30 * 1000);
   }
 
-  timeThatisRequested(){
+  timeThatisRequested() {
     let timeNow = new Date();
     timeNow.setHours(timeNow.getHours() + 1);
     return timeNow;
   }
 
- getStationData() {
-  this.hauptbahnhofDataSubscription = this.api.getHbf()
-    .subscribe(data => {
+  getStationData() {
+    this.hauptbahnhofDataSubscription = this.api.getHbf().subscribe((data) => {
       this.stationData = [];
-      for (const d of (data.departures as any)) {
-      if(d.line.productName !== 'Bus' && d.line.productName !== 'U' && d.line.productName !== 'S' && d.line.productName !== 'STR'){
-        this.stationData.push({
-
-          id: d.tripId.replace(/\|/gi,"%7C"),
-          name: d.stop.name,
-          planned: d.plannedWhen,
-          delay: d.delay /60,
-          platform: d.platform,
-          destination: d.destination.name,
-          line: d.line.name.replace(/\D/g, ""),
-          product: d.line.productName,
-          info: d.remarks[0],
-          cancelled: d.cancelled,
-          actual: d.when,
-          operator: d.line.operator.name
-
-        });
+      for (const d of data.departures as any) {
+        if (
+          d.line.productName !== 'Bus' &&
+          d.line.productName !== 'U' &&
+          d.line.productName !== 'S' &&
+          d.line.productName !== 'STR'
+        ) {
+          this.stationData.push({
+            id: d.tripId.replace(/\|/gi, '%7C'),
+            name: d.stop.name,
+            planned: d.plannedWhen,
+            delay: d.delay / 60,
+            platform: d.platform,
+            destination: d.destination.name,
+            line: d.line.name.replace(/\D/g, ''),
+            product: d.line.productName,
+            info: d.remarks[0],
+            cancelled: d.cancelled,
+            actual: d.when,
+            operator: d.line.operator.name,
+          });
+        }
       }
-    }
     });
-}
+  }
 
-  identifyIfEqual(item:any) {   //IMPORTANT Refresh data only if there is any change
+  identifyIfEqual(item: any) {
+    //IMPORTANT Refresh data only if there is any change
     return item.id;
   }
 
-  openDetails(id:any, product:any, line:any, operator:string){
-    this.name = product + " "+ line
-    this.dialog.open(HauptbahnhofDetailsComponent,
-      {
-        width: '600px',
-        data: {id: id,
-                name: this.name,
-                line: line,
-                product: product,
-                operator: operator
-              },
-      });
+  openDetails(id: any, product: any, line: any, operator: string) {
+    this.name = product + ' ' + line;
+    this.dialog.open(HauptbahnhofDetailsComponent, {
+      width: '600px',
+      data: {
+        id: id,
+        name: this.name,
+        line: line,
+        product: product,
+        operator: operator,
+      },
+    });
   }
 }
